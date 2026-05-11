a4c933b588d3fa31f8e457463654660d30c909c8
aisyahesahh54 <aisyahesahh54@gmail.com>
2026-05-11 14:43:46 +0700

    Update open_meteo_api_test.dart

--- packages/open_meteo_api/test/open_meteo_api_test.dart
+++ packages/open_meteo_api/test/open_meteo_api_test.dart
@@ -1,12 +1,8 @@
-import 'package:flutter_test/flutter_test.dart';
 
-import 'package:open_meteo_api/open_meteo_api.dart';
+import 'package:test/test.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('simple test', () {
+    expect(1 + 1, 2);
   });
 }
